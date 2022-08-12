3be62193470c5eddd256f22ef65e81b4d2548c9a
Leonardo Vilarinho <leonardo@increazy.com>
2022-08-12 17:21:56 -0300

    corrigindo problema de desconto com limtie por cliente

--- Increazy/Checkout/Controller/Payment/Cancel.php
+++ Increazy/Checkout/Controller/Payment/Cancel.php
@@ -43,9 +43,33 @@ class Cancel extends Controller
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $objectManager->get('Magento\Framework\Registry')->register('isSecureArea', true);
         $this->order->loadByIncrementId($body->order_id);
+
         if (stripos($this->order->getPayment()->getMethod(), 'increazy') !== false) {
             $infos = $this->order->getPayment()->getAdditionalInformation();
             if (!isset($infos['infos']['pay_method'])) {
+                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+
+                if ($code = $this->order->getCouponCode()) {
+                    $couponModel = $objectManager->get('Magento\SalesRule\Model\Coupon');
+                    $coupon = $couponModel->loadByCode($code);
+                    $coupon->setTimesUsed($coupon->getTimesUsed() - 1);
+                    $coupon->save();
+
+                    if ($customerId = $this->order->getCustomerId()) {
+                        $rule = $objectManager->get('Magento\SalesRule\Model\Rule');
+
+                        if ($customerCoupon = $rule->load($coupon->getId())) {
+                            $customerCoupon->setTimesUsed($customerCoupon->getTimesUsed() - 1);
+                            $customerCoupon->save();
+                        }
+
+                        $usageCustomer = $objectManager->get('Magento\SalesRule\Model\ResourceModel\Coupon\Usage');
+                        $usageCustomer->updateCustomerCouponTimesUsed($customerId, $coupon->getId(), false);
+                    }
+                }
+
+
                 // $this->registry->register('isSecureArea','true');
                 $this->order->delete();
                 // $this->registry->unregister('isSecureArea');
